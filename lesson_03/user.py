aef53ead4a54d50aa1de02c0af2ed47f6e5e66db
Roman <romasan28@gmail.com>
2026-01-20 23:26:52 +0300

    Update user.py

--- lesson_03/user.py
+++ lesson_03/user.py
@@ -1,13 +1,13 @@
-class user:
+class User:
     def __init__ (self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
         
     def get_first_name(self):
-        return self.first_name
+        print (self.first_name)
     
     def get_last_name(self):
-        return self.last_name
+        print (self.last_name)
     
     def get_firlast_name(self):
-        return f"user: {self.first_name}, {self.last_name}"
+        print( f"User: {self.first_name}, {self.last_name}")
